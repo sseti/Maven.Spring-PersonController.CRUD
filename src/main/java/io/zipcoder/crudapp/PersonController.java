62d76614befb974b3420ea4b0a2affdee98590b0
Sandy Setiawan <ssetiawan@zipcodes-MBP-3.fios-router.home>
2020-04-01 15:35:22 -0400

    making a commit

---
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -0,0 +1,11 @@
+package io.zipcoder.crudapp;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+    Person createPerson(Person p) {
+        return null;
+    }
+
+}
